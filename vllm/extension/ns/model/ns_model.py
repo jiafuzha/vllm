a2fdaff10337ff2166e879762edca6ecfe63c005
Jiafu Zhang <jiafu.zhang@intel.com>
2024-04-25 09:56:55 +0000

    save
    Signed-off-by: Jiafu Zhang <jiafu.zhang@intel.com>

--- vllm/extension/ns/model/ns_model.py
+++ vllm/extension/ns/model/ns_model.py
@@ -8,6 +8,8 @@ from vllm.config import LoRAConfig
 from vllm.model_executor.layers.linear import LinearMethodBase
 from vllm.model_executor.layers.sampler import Sampler
 
+from inference_engine import Model as IE_Model
+
 class NSModel(nn.Module):
     def __init__(self, config: PretrainedConfig,
                    linear_method: Optional[LinearMethodBase],
@@ -16,8 +18,8 @@ class NSModel(nn.Module):
         self.config = config
         self.linear_method = linear_method
         self.lora_config = lora_config
-
-        self.ns_model = None
+        # TODO: determine pad token
+        self.ie_model = IE_Model(config.name_or_path, max_batch_size=10)
 
         self.sampler = Sampler()
 
@@ -26,7 +28,16 @@ class NSModel(nn.Module):
 
     def load_weights(self, weights):
         assert sum(weights) > 0
-        pass
+        
+        qc = self.linear_method.quant_config
+        self.ie_model.check_and_quantize(weight_dtype=qc.weight_dtype,
+                                         alg=qc.alg,
+                                         group_size=qc.group_size,
+                                         scale_dtype=qc.scale_dtype,
+                                         compute_dtype=qc.compute_dtype,
+                                        )
+        self.ie_model.load_weights()
+
     
 class NSLLamaModel(NSModel):
     def __init__(self, config: LlamaConfig,
